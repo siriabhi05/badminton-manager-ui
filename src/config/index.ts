156daa5503ce3b8a2eb50c14d4be9cddb151dd4f
Abhishek <tech.siri.abhi@gmail.com>
2024-07-31 13:51:39 +0530

    added deployed api url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,1 @@
-// export const API_URL = "http://ec2-13-232-179-43.ap-south-1.compute.amazonaws.com"
-export const API_URL = "http://localhost:80"
+export const API_URL = "http://ec2-13-232-179-43.ap-south-1.compute.amazonaws.com"
